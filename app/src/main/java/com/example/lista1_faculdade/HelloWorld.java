898c325b3da4d02197438c03a3641e330bfd4906
Lucas Bechtlufft <luks_dsk@hotmail.com>
2023-08-05 20:28:28 -0300

    Adicionando tela de sorteio de número indicada pelo usuário, sendo instânciada pela Tela principal

---
+++ app/src/main/java/com/example/lista1_faculdade/HelloWorld.java
@@ -0,0 +1,21 @@
+package com.example.lista1_faculdade;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HelloWorld extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    // Method to handle the button click event
+    public void goToAnotherPage(View view) {
+        // Replace "AnotherActivity" with the actual name of your destination activity
+        Intent intent = new Intent(this, Sorteio.class);
+        startActivity(intent);
+    }
+}
